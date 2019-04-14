c332603f8a86e94d624eb3750a9427490ab31ef9
Matthew <mattyg@live.unc.edu>
2019-04-14 07:36:02 -0400

    sync kinda fixed

--- datatransfer/movedata.js
+++ datatransfer/movedata.js
@@ -5,19 +5,12 @@ var fs = require('fs');
 var blockstack = require('blockstack');
 
 exports.put = function() {
-    fs.readFile("./datatransfer/data", {}, function(data) {
- 
-        let thing = JSON.parse(data);
-        if (!thing) {
-            console.log("ruh roh");
-        }
-        blockstack.putFile('https://hub.blockstack.org/' + thing.url, data, {sign: false}).then(
-            function(response){
-                
-            }
-        );
-
-            
+    let thing = JSON.parse(fs.readFileSync("./datatransfer/data"));
+    if (!thing) {
+        console.log("ruh roh");
+    }
+    blockstack.putFile(thing.url, thing, {sign: false}).then(
+        function(response){  
         // let thing = JSON.parse(data);
         // console.log(thing);
         // blockstack.putFile('https://hub.blockstack.org/' + thing.url, data, {sign: false}).then(() => {});
@@ -25,16 +18,13 @@ exports.put = function() {
 }
 
 exports.get = function(callback) {
-    fs.readFileSync("./datatransfer/data", {}, function(data) {
-
-            let thing = JSON.parse(data);
+    let thing = JSON.parse(fs.readFileSync("./datatransfer/data"));
           
-            let result = blockstack.getFile('https://hub.blockstack.org/' + thing.url, {decrypt: true, verify: false}).then(callback);
+        let result = blockstack.getFile('https://hub.blockstack.org/' + thing.url, {decrypt: true, verify: false}).then(callback);
 
     /*
         Returning result is not an option. blockstack.getFile uses asyncronous callback. therfore you must handle its outcomeds in its callback. in order for a caller of this function to be permitted to access guranteed
         data they must, too, pass in a callback.
     */
     //return result;
-    });
 }
