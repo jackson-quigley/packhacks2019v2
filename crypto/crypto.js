c1666d3fd05b9bde557c4b7b0d57305d3feeb028
nosyarg <york17a@ncssm.edu>
2019-04-15 09:04:10 -0400

    trying crypto

--- crypto/crypto.js
+++ crypto/crypto.js
@@ -9,8 +9,17 @@ dataobj['file'] = "file";
 dataobj['url'] = fs.readFileSync("name",'utf-8');
 var filepath = dataobj['file'];
 var file = fs.readFileSync(filepath,'ascii');
-dataobj['date'] = (new Date(Date.now())).toLocaleString("en-US");
-dataobj['hash'] = sjcl.codec.hex.fromBits((new sjcl.hash.sha256()).update(file).finalize());
+dataobj['date'] = (new Date(min(Date.now(),dataobj['date']))).toLocaleString("en-US");
+var oldhash = dataobj['hash'];
+newhash = sjcl.codec.hex.fromBits((new sjcl.hash.sha256()).update(file).finalize());
+if(newhash === oldhash)
+{
+        dataobj['changed'] = 'unchanged'
+}
+else
+{
+        dataobj['changed'] = 'changed'
+}
 fs.writeFileSync('datatransfer/data',JSON.stringify(dataobj));
 return dataobj;
 }
